b84b4997c802f0f7f90390e282403a3451124897
alvseven <alv7contato@outlook.com>
2025-09-05 13:44:23 -0300

    chore: update tsconfig, add internal flag

--- types/index.d.ts
+++ types/index.d.ts
@@ -328,3 +328,18 @@ export type TrackingPartnerFee = {
     transaction_hash: string;
     completed_at: string;
 };
+
+
+/**
+ * @internal
+ * Internal API client interface that resources use to make HTTP requests.
+ * This interface is not exposed to SDK users.
+ */
+/** @internal */
+export interface InternalApiClient {
+    get: <T>(path: string) => Promise<BlindpayApiResponse<T>>;
+    post: <T>(path: string, body: Record<string, unknown>) => Promise<BlindpayApiResponse<T>>;
+    put: <T>(path: string, body: Record<string, unknown>) => Promise<BlindpayApiResponse<T>>;
+    patch: <T>(path: string, body: Record<string, unknown>) => Promise<BlindpayApiResponse<T>>;
+    delete: <T>(path: string, body?: Record<string, unknown>) => Promise<BlindpayApiResponse<T>>;
+}
